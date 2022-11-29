b9138a524590f15cb3a2c012d8be7284cf9802a3
sigau <debaeckergautier@gmail.com>
2022-11-29 16:28:27 +0100

    update README

--- useful_stuff/easicmd-completion.bash
+++ useful_stuff/easicmd-completion.bash
@@ -3,7 +3,7 @@
 _easicmd_completion()
 {
     local opts
-    opts="add_meta add_path build_dico_meta help ichmod idush imkdir irm pull push rm_meta search_by_meta search_name show_meta synchro"
+    opts="add_meta add_path build_dico_meta help ichmod idush imkdir irm pull push rm_meta rm_path search_by_meta search_name show_meta synchro"
     case $COMP_CWORD in
         1)
             COMPREPLY=( $(compgen -W "${opts}" -- "${COMP_WORDS[COMP_CWORD]}") )
